b637077bd308f1371c1d6fb539540c8c4f05a896
Dauth Gurung <dauthgurung@192-168-1-100.tpgi.com.au>
2023-07-11 11:18:47 +1000

    changed the grid order number

--- app/(navbar)/components/homeSignInCard/page.jsx
+++ app/(navbar)/components/homeSignInCard/page.jsx
@@ -12,15 +12,19 @@ const HomeSignInCard = () => {
     return description;
   };
   return (
-    <div className="card w-full   flex flex-col justify-between gap-2">
-      <div className="mb-3 flex flex-col gap-2 bg-white p-3">
+    <div className="card w-full  flex flex-col h-[400px] order-4">
+      <div className=" mb-3 flex flex-col  bg-white p-3 ">
         <h3>Sign in for your best experience</h3>
         <button className="bg-yellow-400 p-2 w-full rounded">
           Sign in securely
         </button>
       </div>
-      <div className="card w-full bg-white p-5 flex flex-col justify-between gap-2 ">
-        <img src={service.image} alt="sign in" />
+      <div className="w-full bg-white p-5 flex flex-col min-h-[200px]">
+        <img
+          src={service.image}
+          alt="sign in"
+          className="h-[200px] object-contain"
+        />
         <p className="text-[12px]">
           {truncateDescription(service.description, 20)}
         </p>
