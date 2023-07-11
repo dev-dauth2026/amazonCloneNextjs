7e59f615b3c39e4e45ca92b1767030804a000d42
Dauth Gurung <dauthgurung@192-168-1-100.tpgi.com.au>
2023-07-11 11:06:00 +1000

    taiwind css removed and add normal css property for grid layout

--- app/page.js
+++ app/page.js
@@ -13,46 +13,21 @@ import Image from "next/image";
 import HomeServiceCategoryList from "./(navbar)/components/homeServiceCategoryCard/homeCategoryList";
 import joinPrime from "../public/homeProductCategory/joinPrime.jpeg";
 import HomePrimeDealList from "./(navbar)/components/homePrimeDealList/page";
+import "./globals.css";
 const Home = () => {
   const [showIcons, setShowIcons] = useState(false);
-  const [highestDiscountedProduct, setHighestDiscountedProduct] =
-    useState(null);
-
-  // Find the product with the highest discounted price
-  useEffect(() => {
-    const highestProduct = ProductList.reduce((prevProduct, currentProduct) => {
-      return currentProduct.discountedPrice > prevProduct.discountedPrice
-        ? currentProduct
-        : prevProduct;
-    });
-
-    setHighestDiscountedProduct(highestProduct);
-  }, []);
 
   return (
     <main className="cardSection w-screen h-auto   absolute md:top-[400px] top-[350px] z-50">
-      <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-3 p-4">
+      <div className="homeCards">
         {/* Product deal starts */}
         <HomeDealCategoryCard />
-        {/* Products deal ends  */}
         {/* sign in starts  */}
         <HomeSignInCard />
-        {/* sign in end  */}
         {/* HomePage Services items starts  */}
-
-        {HomeServiceCategoryList.map((item) => {
-          if (item.id === 2) {
-            return <HomeServiceCategoryCard {...item} key={item.id} />;
-          } else {
-            return null;
-          }
-        })}
-        {/* HomePage Services items starts  */}
-
+        <HomeServiceCategoryCard />
         {/* Home Prime Deal List Starts   */}
-
-        <HomePrimeDealList title="Prime Deal" />
-        <HomePrimeDealList title="Try Prime Free for 30 days" />
+        <HomePrimeDealList />
       </div>
 
       <Footer />
